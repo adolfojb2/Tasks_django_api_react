4c100de601f2e3fccefc7d6df2106a01ebec3a28
adolfojb2 <adolfojb2@gmail.com>
2024-07-04 11:14:28 -0500

    Documentación automatica de la API con coreapi.

--- Backend/django_crud_api/settings.py
+++ Backend/django_crud_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'coreapi',
     'tasks',
 ]
 
@@ -130,3 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
 
 ]
+
+REST_FRAMEWORK = {
+    ...: ...,
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
--- Backend/tasks/urls.py
+++ Backend/tasks/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from tasks import views
 
@@ -7,7 +8,8 @@ router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskView, 'tasks')
 
 urlpatterns = [
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('docs/', include_docs_urls(title='Tasks API'))
 ]
 
 
