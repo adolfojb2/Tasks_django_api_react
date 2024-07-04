636eb1b24ef063a0a0182ab0f191e34f621f58ca
adolfojb2 <adolfojb2@gmail.com>
2024-07-04 09:02:12 -0500

    Configuración de panel admin y creación del archivo gitignore.

--- Backend/tasks/models.py
+++ Backend/tasks/models.py
@@ -6,4 +6,6 @@ class Task(models.Model):
     title = models.CharField(max_length=200) 
     description = models.TextField(blank=True)
     done = models.BooleanField(default=False)
-    
+
+    def __str__(self):
+        return self.title
