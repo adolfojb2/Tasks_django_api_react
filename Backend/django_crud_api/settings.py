d679cadf109df3f0616d2d2acf48e51bfaa02995
adolfojb2 <adolfojb2@gmail.com>
2024-07-04 08:44:19 -0500

    Configuración djangorestframework y creación de la tabla Task en la base de datos.

--- Backend/django_crud_api/settings.py
+++ Backend/django_crud_api/settings.py
@@ -37,12 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'tasks',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Servidores que se podrán conectar a Django  (Cors authorization)
+CORS_ALLOWED_ORIGINS = [
+
+]
