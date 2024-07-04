2fae815d8998ac034e0d3c0540909cc3e38711d7
adolfojb2 <adolfojb2@gmail.com>
2024-07-04 10:57:55 -0500

    Cración del crud y la API con djangorestframework

--- Backend/tasks/views.py
+++ Backend/tasks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import TaskSerializer
+from .models import Task
+
 
 # Create your views here.
+class TaskView(viewsets.ModelViewSet):
+        queryset = Task.objects.all()
+        serializer_class = TaskSerializer
+        
